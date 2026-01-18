b99d9375d46849a06dd98163fd5c99891199e3f8
Mariabel17 <maribelml17@gmail.com>
2026-01-18 18:03:03 +0000

    work in progress 2

--- src/pages/CretateContact.jsx
+++ src/pages/CretateContact.jsx
@@ -2,6 +2,38 @@
 
 export const CreateContactPage = () => {
     return (
-        <h1>ESTA ES LA PÁGINA DE CREAR EL CONTACTO</h1>
+        <>
+        <div className="text-center">
+        <h1>Create Contacts</h1>
+        </div>
+        
+        <div className="container">
+            <div className="row justify-content-center">
+            <div className="col-md-8">
+      <form>
+        <div className="mb-3">
+          <label for="name" className="form-label">Name</label>
+          <input type="text" className="form-control" id="name"/>
+        </div>
+        <div className="mb-3">
+          <label for="address" className="form-label">Address</label>
+          <input type="text" className="form-control" id="address"/>
+        </div>
+        <div className="mb-3">
+          <label for="email" className="form-label">Email</label>
+          <input type="email" className="form-control" id="email" required/>
+        </div>
+        <div className="mb-3">
+          <label for="telefono" className="form-label"> Phone </label>
+          <input type="tel" id="telefono" className="form-control"  placeholder="+34 612 345 678" pattern="(\+34)?[0-9]{9}"required/>
+        </div>
+        <button type="submit" className="btn btn-primary">Submit</button>
+      </form>
+    </div>
+  </div>
+</div>
+
+</>
+
     )
 }
